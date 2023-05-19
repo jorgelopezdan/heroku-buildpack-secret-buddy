63d89f887718f158b47b9f05f9d4c8e2e13c34e8
Alfonso Campodonico <acampodonico@heroku.com>
2023-05-19 13:29:16 -0300

    First iteration of no downtime rotation.

--- secret-buddy-buildpack.go
+++ secret-buddy-buildpack.go
@@ -11,20 +11,37 @@ type ConsolidatedSecret struct {
 	Previous map[string]string `json:"previous"` // Previous secret values, useful for rollbacks
 }
 
-func ExportEnvVarsFromMap(env string) (map[string]string, error) {
+func ExportEnvVarsFromMap(env string, rules map[string]string) (map[string]string, error) {
 	var consolidatedSecret ConsolidatedSecret
 	err := json.Unmarshal([]byte(env), &consolidatedSecret)
+	var envVars map[string]string
+	envVars = consolidatedSecret.Current
+	fmt.Println(envVars)
+
+	for key, value := range rules {
+		var previousValue = "previous." + key
+		if value == previousValue {
+			envVars[key] = consolidatedSecret.Previous[key]
+		}
+	}
 	if err != nil {
 		return nil, err
-
 	}
-
-	return consolidatedSecret.Current, err
+	return envVars, nil
 }
 
 func GetEnvVar(envVarName string) (string, error) {
 	envVarValue := os.Getenv(envVarName)
 	return envVarValue, nil
+}
+
+func ParseRules(ruleString string) (map[string]string, error) {
+	var rules map[string]string
+	err := json.Unmarshal([]byte(ruleString), &rules)
+	if err != nil {
+		return nil, err
+	}
+	return rules, err
 }
 
 func main() {
@@ -34,14 +51,26 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	consolidatedSecret, err := ExportEnvVarsFromMap(envVar)
+
+	variableX, err := GetEnvVar("HEROKU_SECRETS_CONFIG")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	rules, err := ParseRules(variableX)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	consolidatedSecret, err := ExportEnvVarsFromMap(envVar, rules)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	for key, value := range consolidatedSecret {
 		fmt.Printf("export %s=%v\n", key, value)
-
 	}
 
 }
